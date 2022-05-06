13cd96cf570f2e32ee47dfc35e493da55d1f7cc2
Chan Bingjie <jienet888@gmail.com>
2022-05-06 12:30:41 +0800

    EntityMapper class to be solely responsible for mapping

--- mapper.py
+++ mapper.py
@@ -1,19 +1,41 @@
-from typing import List
+from abc import ABC, abstractmethod
+from typing import Any, Dict, List
+
 from pydash import objects
 
 from custom_types import Mapping
 
-class MappingError(Exception):
-    pass
+# class MappingError(Exception):
+#     pass
 
 
-class EntityMapper:
-    def __init__(self, mappings: List[Mapping]):
-        self.mappings = mappings
+class EntityMapper(ABC):
+    @staticmethod
+    @abstractmethod
+    def get_mapping() -> List[Mapping]:
+        """
+        Abstract static property containing the list of mappings, implemented as a
+        method as a workaround.
 
-    def transform(self, data: dict) -> dict:
+        Returns:
+            List[Mapping]: List of `(new_field, old_field, transform_func)` mappings
+        """        
+        raise NotImplementedError()
+
+
+    def transform(self, data: Dict[Any, Any]) -> Dict[Any, Any]:
+        """
+        Performs the JSON transformation from raw objects into our custom mappings.
+
+
+        Args:
+            data (Dict[Any, Any]): The original raw data object.
+
+        Returns:
+            Dict[Any, Any]: The transformed data object.
+        """ 
         result = {}
-        for new_key, old_key, transform in self.mappings:
+        for new_key, old_key, transform in self.get_mapping():
             old_val = objects.get(data, old_key)
             
             # Uncommented this snippet, some values may be intentionally left as null
