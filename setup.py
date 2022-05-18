84dc23d60833ffe7c8c7242d9d4deefdec83b254
gchuntec <gchuntec@test.com>
2022-05-18 14:07:49 +0800

    update setup with requirements

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
-import pathlib
 import os
 import setuptools
 
-HERE = pathlib.Path(__file__).parent
 VERSION = os.environ.get("CI_COMMIT_TAG", "0.0.0")
-README = (HERE / "README.md").read_text()
+with open("README.md", "r", encoding="utf-8") as file:
+    LONG_DESCRIPTION = file.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as file:
+    REQUIREMENTS = [
+        line.strip() for line in file.readlines() if line and not line.startswith("#")
+    ]
 
 setuptools.setup(
     name="pydantic-model-parser",
     version=VERSION,
     description="A simple package to transform/map dictionaries, before parsing it into Pydantic.",
-    long_description=README,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
     ],
-    install_requires=["pydantic >= 1.9.0", "pydash >= 5.0.0"],
+    install_requires=REQUIREMENTS,
     python_requires=">=3.8",
     packages=setuptools.find_packages(),
     use_pipfile=True,
