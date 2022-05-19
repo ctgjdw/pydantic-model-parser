8eedc4f955eea48de799c0af20be804b03f8204f
gchuntec <gchuntec@test.com>
2022-05-19 10:40:11 +0800

    remove req.txt

--- setup.py
+++ setup.py
@@ -2,13 +2,9 @@ import os
 import setuptools
 
 VERSION = os.environ.get("CI_COMMIT_TAG", "0.0.0")
+
 with open("README.md", "r", encoding="utf-8") as file:
     LONG_DESCRIPTION = file.read()
-
-with open("requirements.txt", "r", encoding="utf-8") as file:
-    REQUIREMENTS = [
-        line.strip() for line in file.readlines() if line and not line.startswith("#")
-    ]
 
 setuptools.setup(
     name="pydantic-model-parser",
@@ -20,7 +16,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
     ],
-    install_requires=REQUIREMENTS,
+    install_requires=["pydantic >= 1.9.0", "pydash >= 5.0.0"],
     python_requires=">=3.8",
     packages=setuptools.find_packages(),
     use_pipfile=True,
