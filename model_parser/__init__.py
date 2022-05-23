564824053714541c03cc0da7512f9db72f6d95cf
gchuntec <gchuntec@test.com>
2022-05-23 10:01:52 +0800

    update package imports

--- model_parser/__init__.py
+++ model_parser/__init__.py
@@ -2,4 +2,9 @@ from pydantic import ValidationError
 
 from model_parser.pydantic_parser import Parser
 from model_parser.mapper import BaseMapper
-from model_parser.custom_types import MappingError, Mapping
+from model_parser.custom_types import (
+    MappingError,
+    Mapping,
+    TransformFuncError,
+    TransformFunc,
+)
