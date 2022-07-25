bc2ed86ef7e7014d474bdc6331770f69463915f4
gohchunteck@hotmail.com <gohchunteck@hotmail.com>
2022-07-25 23:31:56 +0800

    updated with tests

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,4 @@
+import unittest
 from typing import List, Optional
 
 from pydantic import BaseModel
@@ -15,7 +16,7 @@ class User(BaseModel):
 
 class Comment(BaseModel):
     id: int
-    comment_str: str
+    comment_str: Optional[str]
     user: User
     address_str: str
     comment_backup: Optional[str]
@@ -46,47 +47,98 @@ class CommentMapper(BaseMapper):
             # user_tags does not exist in input dict, use default_val instead in new dict
             Mapping("user_tags", "user.tags", default_val=[]),
             Mapping(
-                "comment_backup",
-                "comment_backup",
-                default_val_func=lambda data: data.get("comment_str"),
+                "comment_id",
+                "comment_id",
+                default_val_func=lambda data: int(data.get("comment_id_str"))
+                if data.get("comment_id_str")
+                else None,
             ),
         ]
 
 
-data = {
-    "id": 1,
-    "comment_str": "HelloWorld",
-    "user_id": "2",
-    "user_name": "bob",
-    "street_name": "123 St",
-    "block_name": "244",
-    "unit_name": "10-123",
-}
-data_list = [data, data]
-parser = Parser(Comment, CommentMapper)
+MAPPER = CommentMapper()
+PARSER = Parser(Comment, CommentMapper)
 
-# transform dict to new_dict (after mapping)
-MAPPED_COMMENT_DICT = CommentMapper.transform(data)
 
-# parse into a Comment entity
-try:
-    COMMENT = parser.parse(data)
-except TransformFuncError:
-    print("Error applying transform_func!")
-except DefaultValFuncError:
-    print("Error applying default_val_func!")
-except PydanticError:
-    print("Validation Error when parsing into Pydantic")
+def get_input():
+    return {
+        "id": 1,
+        "comment_id_str": "1",
+        "user_id": "2",
+        "user_name": "bob",
+        "street_name": "123 St",
+        "block_name": "244",
+        "unit_name": "10-123",
+    }
 
-# parse into a list of Comment entities
-try:
-    COMMENTS = parser.parse(data_list)
-except TransformFuncError:
-    print("Error applying transform_func!")
-except DefaultValFuncError:
-    print("Error applying default_val_func!")
-except PydanticError:
-    print("Validation Error when parsing into Pydantic")
 
-print(MAPPED_COMMENT_DICT)
-print(COMMENT)
+def get_output():
+    return {
+        "id": 1,
+        "comment_str": None,
+        "user": {"id": 2, "name": "bob", "first_name": None, "tags": []},
+        "address_str": "123 St 244 10-123",
+        "comment_id": 1,
+    }
+
+
+class TestMapperTransformFunc(unittest.TestCase):
+    def setUp(self):
+        self.mapper = MAPPER
+        self.input = get_input()
+        self.output = get_output()
+
+    def test_success(self):
+        self.assertDictEqual(self.mapper.transform(self.input), self.output)
+
+    def test_empty_input_dict_success(self):
+        self.input = {}
+        self.output = self.output = {
+            "id": None,
+            "comment_str": None,
+            "user": {"id": None, "name": None, "first_name": None, "tags": []},
+            "address_str": None,
+            "comment_id": None,
+        }
+        self.assertDictEqual(self.mapper.transform(self.input), self.output)
+
+    def test_transform_func_error(self):
+        self.input["user_id"] = "1a"
+        with self.assertRaises(TransformFuncError):
+            self.mapper.transform(self.input)
+
+    def test_default_func_error(self):
+        self.input["comment_id_str"] = "1a"
+        with self.assertRaises(DefaultValFuncError):
+            self.mapper.transform(self.input)
+
+
+class TestParser(unittest.TestCase):
+    def setUp(self):
+        self.parser = PARSER
+        self.input = get_input()
+        self.input_list = [get_input() for _ in range(3)]
+        self.output = Comment.parse_obj(get_output())
+        self.output_list = [Comment.parse_obj(get_output()) for _ in range(3)]
+
+    def test_parse_single_dict_success(self):
+        result = self.parser.parse(self.input)
+        self.assertEqual(result, self.output)
+
+    def test_parse_many_dicts_success(self):
+        result = self.parser.parse(self.input_list)
+        self.assertEqual(result, self.output_list)
+
+    def test_parse_non_optional_field_pydantic_error(self):
+        self.input["id"] = None
+        with self.assertRaises(PydanticError):
+            self.parser.parse(self.input)
+
+    def test_parse_field_type_mismatch_pydantic_error(self):
+        self.input["id"] = "id_str"
+        with self.assertRaises(PydanticError):
+            self.parser.parse(self.input)
+
+
+if __name__ == "__main__":
+    unittest.main()
